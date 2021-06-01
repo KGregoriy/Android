f4cc9e3e4b9154d7d696720d5e3ea5c3a46e4294
Grigorii <Kalinin>
2021-06-01 22:50:40 +0600

    Добавление второго activity

--- Lesson_1/app/src/main/java/com/example/lesson_1/MainActivity.java
+++ Lesson_1/app/src/main/java/com/example/lesson_1/MainActivity.java
@@ -2,13 +2,32 @@ package com.example.lesson_1;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    Button btnOpen;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btnOpen = (Button) findViewById(R.id.buttonOpenActivity);
+        btnOpen.setOnClickListener(this);
+    }
+
+    public void onClick(View v){
+        switch (v.getId()){
+            case R.id.buttonOpenActivity:
+                Intent intent = new Intent(this, MainActivity2.class);
+                startActivity(intent);
+                break;
+            default:
+                break;
+        }
     }
 }
